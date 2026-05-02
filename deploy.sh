2aadd3f470fe5aed063c7b3ce13edcc5e79581ed
roychshao <roych.shao@gmail.com>
2026-05-03 01:05:32 +0800

    [feat] tailscale

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-kubectl delete deployment hermes-agent
 kubectl apply -f ./k8s/secrets/hermes-sealed-secret.yaml
+kubectl apply -f ./k8s/rbac.yaml
 kubectl apply -f ./k8s/configmap.yaml
 kubectl apply -f ./k8s/pvc.yaml
 kubectl apply -f ./k8s/deployment.yaml
